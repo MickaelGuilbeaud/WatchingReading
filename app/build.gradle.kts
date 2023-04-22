f1bc53b9e8080e8734a04f4f409d2cad74339bae
MickaelGuilbeaud <mguilbeaud@medium.com>
2023-04-22 15:24:59 +0200

    Move libraries to Gradle version catalog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,17 +50,16 @@ android {
 }
 
 dependencies {
-    val composeVersion = "1.3.3"
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha06")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+    implementation(libs.androidx.activityCompose)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.lifecycleRuntime)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.uiToolingPreview)
+    implementation(libs.compose.material3)
+    testImplementation(libs.jUnit)
+    androidTestImplementation(libs.androidx.test.espressoCore)
+    androidTestImplementation(libs.androidx.test.jUnit)
+    androidTestImplementation(libs.compose.uiTestJUnit4)
+    debugImplementation(libs.compose.uiTooling)
+    debugImplementation(libs.compose.uiTestManifest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,4 +16,39 @@ dependencyResolutionManagement {
 
 rootProject.name = "WatchingReading"
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            // region Versions
+            version("compose", "1.3.3")
+            // endregion
+
+            // region Dependencies
+            // region AndroidX
+            library("androidx.activityCompose", "androidx.activity", "activity-compose").version("1.6.1")
+            library("androidx.core", "androidx.core", "core-ktx").version("1.9.0")
+            library("androidx.lifecycleRuntime", "androidx.lifecycle", "lifecycle-runtime-ktx").version("2.5.1")
+            library("androidx.test.espressoCore", "androidx.test.espresso", "espresso-core").version("3.5.1")
+            library("androidx.test.jUnit", "androidx.test.ext", "junit").version("1.1.5")
+            // endregion
+            // region Compose
+            library("compose.material3", "androidx.compose.material3", "material3").version("1.1.0-alpha06")
+            library("compose.ui", "androidx.compose.ui", "ui").versionRef("compose")
+            library("compose.uiToolingPreview", "androidx.compose.ui", "ui-tooling-preview").versionRef("compose")
+            library("compose.uiTooling", "androidx.compose.ui", "ui-tooling").versionRef("compose")
+            library("compose.uiTestManifest", "androidx.compose.ui", "ui-test-manifest").versionRef("compose")
+            library("compose.uiTestJUnit4", "androidx.compose.ui", "ui-test-junit4").versionRef("compose")
+            // endregion
+            // region Tests
+            library("jUnit", "junit", "junit").version("4.13.2")
+            // endregion
+            // endregion
+
+            // region Plugins
+
+            // endregion
+        }
+    }
+}
+
 include(":app")
