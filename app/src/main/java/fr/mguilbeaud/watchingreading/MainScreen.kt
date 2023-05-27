a71be523df103e3ed5b44f81f0388b407ebeda5b
MickaelGuilbeaud <mguilbeaud@medium.com>
2023-05-27 20:01:21 +0200

    First simple implementation of the NavigationBar.

---
+++ app/src/main/java/fr/mguilbeaud/watchingreading/MainScreen.kt
@@ -0,0 +1,93 @@
+package fr.mguilbeaud.watchingreading
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import fr.mguilbeaud.watchingreading.ui.theme.WatchingReadingTheme
+
+@Composable
+fun MainScreen() {
+    var selectedNavigationBarItem by remember { mutableStateOf(NavigationBarItem.Animes) }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
+        bottomBar = {
+            MainNavigationBar(
+                navigationBarItems = NavigationBarItem.values().toList(),
+                selectedNavigationBarItem = selectedNavigationBarItem,
+                onNavigationBarItemSelected = { selectedNavigationBarItem = it },
+            )
+        },
+    ) { paddingValues ->
+        Text(
+            text = stringResource(selectedNavigationBarItem.nameResId) + " coming soon!",
+            modifier = Modifier
+                .padding(paddingValues),
+        )
+    }
+}
+
+private enum class NavigationBarItem(
+    @StringRes val nameResId: Int,
+    @DrawableRes val iconResId: Int,
+) {
+    Animes(
+        nameResId = R.string.navigation_bar_animes,
+        iconResId = R.drawable.ic_tv_24,
+    ),
+    Mangas(
+        nameResId = R.string.navigation_bar_mangas,
+        iconResId = R.drawable.ic_menu_book_24,
+    ),
+    Search(
+        nameResId = R.string.navigation_bar_search,
+        iconResId = R.drawable.ic_search_24,
+    ),
+    News(
+        nameResId = R.string.navigation_bar_news,
+        iconResId = R.drawable.ic_newspaper_24,
+    ),
+    Settings(
+        nameResId = R.string.navigation_bar_settings,
+        iconResId = R.drawable.ic_settings_24,
+    ),
+}
+
+@Composable
+private fun MainNavigationBar(
+    navigationBarItems: List<NavigationBarItem>,
+    selectedNavigationBarItem: NavigationBarItem,
+    onNavigationBarItemSelected: (NavigationBarItem) -> Unit,
+) {
+    NavigationBar {
+        navigationBarItems.forEach { navigationBarItem ->
+            NavigationBarItem(
+                selected = navigationBarItem == selectedNavigationBarItem,
+                onClick = { onNavigationBarItemSelected(navigationBarItem) },
+                icon = {
+                    Icon(
+                        painter = painterResource(navigationBarItem.iconResId),
+                        contentDescription = stringResource(navigationBarItem.nameResId),
+                    )
+                },
+                label = { Text(stringResource(navigationBarItem.nameResId)) },
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainScreenPreview() {
+    WatchingReadingTheme {
+        MainScreen()
+    }
+}
